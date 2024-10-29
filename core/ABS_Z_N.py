32dff05db72030a675c1fc2386b19d55e56d58c1
Teika7778 <max2005swet@gmail.com>
2024-10-29 09:47:51 +0300

    Фиксы: 1. теперь ABS_Z_N написано капсом, что соответствует норме, а также принимает массив 2. исправлены тесты

--- core/ABS_Z_N.py
+++ core/ABS_Z_N.py
@@ -1,7 +1,11 @@
 from data_types import *
 import copy
 
-def abs_z_n(integer):
-    if not isinstance(integer, Integer): #проверка типа
-            raise ValueError("Invalid data type: must be integer")
-    return [copy.deepcopy(integer.natural)] #возвращаем массив
+
+def ABS_Z_N(args):
+    if not len(args) == 1:
+        raise ValueError("Improper arguments: function takes only 1 arg")
+    integer = args[0]
+    if not isinstance(integer, Integer):  # проверка типа
+        raise ValueError("Invalid data type: must be integer")
+    return [copy.deepcopy(integer.natural)]  # возвращаем массив
--- core/tests/ABS_Z_N_test.py
+++ core/tests/ABS_Z_N_test.py
@@ -1,27 +1,30 @@
 import unittest
 from data_types import *
-from ABS_Z_N import abs_z_n
+from core.ABS_Z_N import ABS_Z_N
 
-class abs_z_n_test(unittest.TestCase):
+
+class ABS_Z_N_test(unittest.TestCase):
     def test_valid_integer(self):
         natural = Natural([1, 2, 3])
         integer = Integer(natural, is_positive=True)
-        result = abs_z_n(integer)
-        self.assertEqual(result[0].numbers, natural.numbers) # Проверяем, что при создании значения в result[0] совпадают с natural
+        result = ABS_Z_N([integer])
+        self.assertEqual(result[0].numbers,
+                         natural.numbers)  # Проверяем, что при создании значения в result[0] совпадают с natural
 
     def test_invalid_type(self):
         with self.assertRaises(ValueError):
-            abs_z_n(Rational(Integer(Natural([2])), Natural([1])))
+            ABS_Z_N([Rational(Integer(Natural([2])), Natural([1]))])
 
     def test_deepcopy(self):
         natural = Natural([1, 2, 3])  # Создаем объект Natural
         integer = Integer(natural, is_positive=True)
-        result = abs_z_n(integer)  # Получаем массив с глубокой копией
-        result[0].numbers.append(4)   # Изменяем результат, добавляя новое значение
+        result = ABS_Z_N([integer])  # Получаем массив с глубокой копией
+        result[0].numbers.append(4)  # Изменяем результат, добавляя новое значение
 
         # Проверяем, что измененный результат не равен оригинальному natural
         self.assertNotEqual(result[0].numbers, natural.numbers)  # Проверяем, что numbers в result изменились
         self.assertEqual(natural.numbers, [1, 2, 3])  # Проверяем, что natural остался прежним
+
 
 if __name__ == '__main__':
     unittest.main()

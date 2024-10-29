178ba581cded45fce9261cb3c4aa0ed0f343e78c
grebenshichovartem <artem.grebenshichov@gmail.com>
2024-10-29 13:42:28 +0300

    MUL_Nk_N

--- core/MUL_Nk_N.py
+++ core/MUL_Nk_N.py
@@ -6,14 +6,14 @@ import copy
 class MUL_Nk_N(gm.AbstractModule):
     def execute(self, args):
         if len(args) != 2:
-            raise ValueError()
+            raise ValueError("Improper arguments: function takes only 2 arg")
         if not (isinstance(args[0], Natural) and isinstance(args[1], Natural)):
-            raise ValueError()
+            raise ValueError("Invalid data type: must be natural")
         result = copy.deepcopy(args[0])
         for i in range(len(args[1].numbers)):  # проходит по списку цифр натурального числа k
             result.numbers = args[1].numbers[i] * 10**i * [0] + result.numbers  # Дописывается число нулей в соответсвии
             # с разрядом и цифрой данного разряда
-        return result
+        return [result]
 
     def reference(self) -> str:
         pass

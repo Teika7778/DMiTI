b0f91cff7235cc09d63ea2831ee68016617fb5f2
Anastasiya Dorogushina <144445609+nastyanesty@users.noreply.github.com>
2024-10-31 22:54:16 +0300

    Update SUB_PP_P_test.py

--- core/tests/SUB_PP_P_test.py
+++ core/tests/SUB_PP_P_test.py
@@ -23,11 +23,11 @@ class SUB_PP_P_test(unittest.TestCase):
         P2 = Polynomial(coef2)
 
         expected_coef = [
-            Rational(Integer(Natural([1]), True), Natural([1])),  # 0
+            Rational(Integer(Natural([0]), True), Natural([1])),  # 0
             Rational(Integer(Natural([1]), True), Natural([1])),  # 1
-            Rational(Integer(Natural([2]), True), Natural([1]))  # 3
+            Rational(Integer(Natural([3]), True), Natural([1]))  # 3
         ]
-        expected_result = Polynomial(expected_coef)     # 12
+        expected_result = Polynomial(expected_coef)     # 310
         result = self.module.execute([P1, P2])
         self.assertEqual(result.str(), expected_result.str())
 

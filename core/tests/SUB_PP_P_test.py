41ef0096fd74a050f1cebc1b19f5a6755264a433
Anastasiya Dorogushina <144445609+nastyanesty@users.noreply.github.com>
2024-10-31 22:01:07 +0300

    SUB_PP_P.py

--- core/tests/SUB_PP_P_test.py
+++ core/tests/SUB_PP_P_test.py
@@ -10,22 +10,22 @@ class SUB_PP_P_test(unittest.TestCase):
     def test_subtract_polynomials(self):
         # создаем полиномы p1 и p2
         coef1 = [
-            Rational(Integer(Natural([1])), Natural([1])),  # 1
-            Rational(Integer(Natural([2])), Natural([1])),  # 2
-            Rational(Integer(Natural([3])), Natural([1]))  # 3
+            Rational(Integer(Natural([1]), true), Natural([1])),  # 1
+            Rational(Integer(Natural([2]), true), Natural([1])),  # 2
+            Rational(Integer(Natural([3]), true), Natural([1]))  # 3
         ]
         P1 = Polynomial(coef1)
 
         coef2 = [
-            Rational(Integer(Natural([1])), Natural([1])),  # 1
-            Rational(Integer(Natural([1])), Natural([1])),  # 1
+            Rational(Integer(Natural([1]), true), Natural([1])),  # 1
+            Rational(Integer(Natural([1]), true), Natural([1])),  # 1
         ]
         P2 = Polynomial(coef2)
 
         expected_coef = [
-            Rational(Integer(Natural([1])), Natural([1])),  # 0
-            Rational(Integer(Natural([1])), Natural([1])),  # 1
-            Rational(Integer(Natural([2])), Natural([1]))  # 3
+            Rational(Integer(Natural([1]), true), Natural([1])),  # 0
+            Rational(Integer(Natural([1]), true), Natural([1])),  # 1
+            Rational(Integer(Natural([2]), true), Natural([1]))  # 3
         ]
         expected_result = Polynomial(expected_coef)     # 12
         result = self.module.execute([P1, P2])
@@ -33,21 +33,21 @@ class SUB_PP_P_test(unittest.TestCase):
 
     def test_subtract_polynomials_with_zero_result(self):
         coef3 = [
-            Rational(Integer(Natural([0])), Natural([1])),  # 0
-            Rational(Integer(Natural([0])), Natural([1])),  # 0
-            Rational(Integer(Natural([0])), Natural([1]))  # 0
+            Rational(Integer(Natural([0]), true), Natural([1])),  # 0
+            Rational(Integer(Natural([0]), true), Natural([1])),  # 0
+            Rational(Integer(Natural([0]), true), Natural([1]))  # 0
         ]
         P3 = Polynomial(coef3)
 
         coef4 = [
-            Rational(Integer(Natural([0])), Natural([1])),  # 0
-            Rational(Integer(Natural([0])), Natural([1])),  # 0
-            Rational(Integer(Natural([0])), Natural([1]))  # 0
+            Rational(Integer(Natural([0]), true), Natural([1])),  # 0
+            Rational(Integer(Natural([0]), true), Natural([1])),  # 0
+            Rational(Integer(Natural([0]), true), Natural([1]))  # 0
         ]
         P4 = Polynomial(coef4)
 
         coeff = [
-            Rational(Integer(Natural([0])), Natural([1]))  #0
+            Rational(Integer(Natural([0]), true), Natural([1]))  #0
         ]
         expected_zero_poly = Polynomial(coeff)  # 0
         result = self.sub_pp.execute([P3, P4])

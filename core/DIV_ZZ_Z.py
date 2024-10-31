a09cd0c1209eabf8cf3ee8320fff0991b049d663
LilFolant <dmitrykashutin_04@mail.ru>
2024-10-31 21:38:49 +0300

    DIV_ZZ_Z с заглушками

---
+++ core/DIV_ZZ_Z.py
@@ -0,0 +1,66 @@
+from core import generic_module as gm
+from data_types import *
+import copy
+from core.ABS_Z_N import ABS_Z_N  # Абсолютная величина числа, результат - натуральное
+from core.POZ_Z_D import POZ_Z_D  # Определение положительности числа
+### раскоменьтить from core.DIV_NN_N import DIV_NN_N #  Неполное частное от деления первого натурального числа на второе с остатком
+from core.ADD_1N_N import ADD_1N_N  # Добавление 1 к натуральному числу
+
+
+#  Частное от деления целого на целое (делитель отличен от нуля)
+class DIV_ZZ_Z(gm.AbstractModule):
+
+    def __init__(self):
+        self.abs_z_n = ABS_Z_N()
+        self.poz_z_d = POZ_Z_D()
+### раскоменьтить       self.div_nn_n = DIV_NN_N()
+        self.add_1n_n = ADD_1N_N()
+
+    def execute(self, args):
+        if len(args) != 2:  # проверка на количество аргументов
+            raise ValueError("Invalid number of arguments")
+        if not (isinstance(args[0], Integer) and isinstance(args[1], Integer)):  # проверка на тип данных
+            raise ValueError("The number isn`t integer")
+        if len(args[1].natural.numbers) == 1 and args[1].natural.numbers[-1] == 0:  # проверка делителя на ноль
+            raise ValueError("The divisor cannot be zero")
+
+        num1 = self.abs_z_n.execute([args[0]])  # получаем абсолютную величину делимого
+        num2 = self.abs_z_n.execute([args[1]])  # получаем абсолютную величину делителя
+### раскоменьтить       num3 = self.div_nn_n.execute([num1[0], num2[1]])  # получаем неполное частное от деления двух натуральных чисел
+
+#/// удалить, когда будет DIV_NN_N
+        test1 = ''
+        test2 = ''
+        for item in num1[0].numbers:
+            test1 += str(item)
+        for item in num2[0].numbers:
+            test2 += str(item)
+        num3 = int(test1[::-1]) // int(test2[::-1])
+        num3 = [int(x) for x in str(num3)]
+        num3 = Natural(num3[::-1])
+        result = copy.deepcopy(Integer(num3))
+#/// удалить, когда будет DIV_NN_N
+
+
+### раскоменьтить       result = copy.deepcopy(Integer(num3[0]))  # Создаем число типа Integer, равное частному
+        # если делимое отрицательное, а делитель положительный -> остаток отрицательный
+        # если делимое и делитель отрицательные -> остаток отрицательный
+        # => если делимое отрицательное, то остаток отрицательный, нужно прибавить 1
+        # частному, чтобы избежать отрицательного остатка
+        if not args[0].is_positive:
+            result.natural = self.add_1n_n.execute([result.natural])[0]
+
+        if not (len(result.natural.numbers) == 1 and result.natural.numbers[-1] == 0):  # если частное не равно нулю
+            # если делимое положительное, а делитель отрицательный
+            if self.poz_z_d.execute([args[0]])[0].numbers == Natural([2]).numbers and\
+                    self.poz_z_d.execute([args[1]])[0].numbers == Natural([1]).numbers:
+                result.is_positive = not result.is_positive  # меняем знак на противоположный
+            # если делимое отрицательное, а делитель положительный
+            if self.poz_z_d.execute([args[0]])[0].numbers == Natural([1]).numbers and\
+                    self.poz_z_d.execute([args[1]])[0].numbers == Natural([2]).numbers:
+                result.is_positive = not result.is_positive  # меняем знак на противоположный
+        # Случаи, когда делимое и делитель имеют один знак, можно не рассматривать, тк смены знака не произойдет
+        return [result]
+
+    def reference(self) -> str:
+        pass
---
+++ core/tests/DIV_ZZ_Z_test.py
@@ -0,0 +1,58 @@
+import unittest
+from core.DIV_ZZ_Z import DIV_ZZ_Z
+from data_types import *
+
+
+class DIV_ZZ_Z_test(unittest.TestCase):
+    def setUp(self):
+        self.module = DIV_ZZ_Z()
+
+    def test_dividing_the_positive_by_the_positive(self):
+        n1 = Integer(Natural([1, 2]))
+        n2 = Integer(Natural([5]))
+        result = self.module.execute([n1, n2])
+        self.assertEqual(result[0].natural.numbers, [4])  # 21 // 5 = 4(ост.1)
+        self.assertEqual(result[0].is_positive, True)
+
+    def test_dividing_the_negative_by_the_negative(self):
+        n1 = Integer(Natural([1, 7]), False)
+        n2 = Integer(Natural([0, 1]), False)
+        result = self.module.execute([n1, n2])
+        self.assertEqual(result[0].natural.numbers, [8])  # -71 // -10 = 7(ост.-1) => -71 // -10 = 8(ост.9)
+        self.assertEqual(result[0].is_positive, True)
+
+    def test_dividing_the_positive_by_the_negative(self):
+        n1 = Integer(Natural([5, 3]), True)
+        n2 = Integer(Natural([8]), False)
+        result = self.module.execute([n1, n2])
+        self.assertEqual(result[0].natural.numbers, [4])  # 35 // -8 = -4(ост.3)
+        self.assertEqual(result[0].is_positive, False)
+
+    def test_dividing_the_negative_by_the_positive(self):
+        n1 = Integer(Natural([2, 2]), False)
+        n2 = Integer(Natural([7, 1]), True)
+        result = self.module.execute([n1, n2])
+        self.assertEqual(result[0].natural.numbers, [2])  # -22 // 17 = -1(ост.-5) => -22 // 17 = -2(ост12)
+        self.assertEqual(result[0].is_positive, False)
+
+    def test_divisor_is_zero(self):
+        n1 = Integer(Natural([5]))
+        n2 = Integer(Natural([0]))
+        with self.assertRaises(ValueError) as context:
+            self.module.execute([n1, n2])  # деление на 0
+        self.assertEqual(str(context.exception), "The divisor cannot be zero")
+
+    def test_invalid_arguments_length(self):
+        with self.assertRaises(ValueError) as context:
+            self.module.execute([])  # неверное количество аргументов
+        self.assertEqual(str(context.exception), "Invalid number of arguments")
+
+    def test_invalid_arguments_type(self):
+        n1 = Natural([1])
+        n2 = Integer(n1)
+        with self.assertRaises(ValueError) as context:
+            self.module.execute([n1, n2])  # неверный тип аргумента
+        self.assertEqual(str(context.exception), "The number isn`t integer")
+
+if __name__ == '__main__':
+    unittest.main()

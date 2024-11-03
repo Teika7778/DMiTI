239940ac4343efa5d2afc52a917b99b1693cf3b9
Anastasiya Dorogushina <144445609+nastyanesty@users.noreply.github.com>
2024-11-03 17:27:12 +0300

    Update ADD_QQ_Q_test.py

--- core/tests/ADD_QQ_Q_test.py
+++ core/tests/ADD_QQ_Q_test.py
@@ -9,42 +9,43 @@ class TestADDQQQ(unittest.TestCase):
 
     def test_add_positive_rationals(self):
         # Тест сложения положительных дробей
-        r1 = Rational(Natural([1]), Natural([2]))  # 1/2
-        r2 = Rational(Natural([1]), Natural([3]))  # 1/3
-        result = self.add_qq_q.execute([r1, r2])
-        self.assertEqual(result[0].numerator.numbers, [5])  # 5 (числитель)
-        self.assertEqual(result[0].denominator.numbers, [6])  # 6 (знаменатель), т.к. 1/2 + 1/3 = 5/6
+        r1 = Rational(Integer(Natural([1]), True), Natural([2]))  # 1/2
+        r2 = Rational(Integer(Natural([1]), True), Natural([3]))  # 1/3
+        result = self.add_qq_q.execute([r1, r2])[0]
+        num = Integer(Natural([5]), True)
+        den = Natural([6])
+        self.assertEqual(result.numerator, num)  # 5 (числитель)
+        self.assertEqual(result.denominator, den)  # 6 (знаменатель), т.к. 1/2 + 1/3 = 5/6
 
     def test_add_negative_rationals(self):
         # Тест сложения отрицательных дробей
-        r1 = Rational(Natural([1]), Natural([2]))  # 1/2
-        r2 = Rational(Natural([1]), Natural([3]))  # 1/3
-        result = self.add_qq_q.execute([-r1, -r2])
-        self.assertEqual(result[0].numerator.numbers, [-5])  # -5 (числитель)
-        self.assertEqual(result[0].denominator.numbers, [6])  # 6 (знаменатель)
+        r1 = Rational(Integer(Natural([1]), False), Natural([2]))  # -1/2
+        r2 = Rational(Integer(Natural([1]), False), Natural([3]))  # -1/3
+        result = self.add_qq_q.execute([r1, r2])[0]
+        num = Integer(Natural([5]), False)
+        den = Natural([6])
+        self.assertEqual(result.numerator, num)  # -5 (числитель)
+        self.assertEqual(result.denominator, den)  # 6 (знаменатель), т.к. -1/2 + -1/3 = -5/6
 
     def test_add_mixed_rationals(self):
         # Тест сложения смешанных дробей
-        r1 = Rational(Natural([1]), Natural([2]))  # 1/2
-        r2 = Rational(Natural([2]), Natural([3]))  # 2/3
-        result = self.add_qq_q.execute([r1, r2])
-        self.assertEqual(result[0].numerator.numbers, [7])  # 7 (числитель)
-        self.assertEqual(result[0].denominator.numbers, [6])  # 6 (знаменатель), т.к. 1/2 + 2/3 = 7/6
+        r1 = Rational(Integer(Natural([1]), True), Natural([2]))  # 1/2
+        r2 = Rational(Integer(Natural([2]), True), Natural([3]))  # 2/3
+        result = self.add_qq_q.execute([r1, r2])[0]
+        num = Integer(Natural([7]), True)
+        den = Natural([6])
+        self.assertEqual(result.numerator, num)  # 7 (числитель)
+        self.assertEqual(result.denominator, den)  # 6 (знаменатель), т.к. 1/2 + 2/3 = 7/6
 
     def test_add_zero_rational(self):
         # Тест сложения дроби с нулем
-        r1 = Rational(Natural([1]), Natural([2]))  # 1/2
-        r2 = Rational(Natural([0]), Natural([1]))  # 0/1 (ноль)
-        result = self.add_qq_q.execute([r1, r2])
-        self.assertEqual(result[0].numerator.numbers, [1])  # 1 (числитель)
-        self.assertEqual(result[0].denominator.numbers, [2])  # 2 (знаменатель)
-
-    def test_invalid_arguments(self):
-        # Тест на неправильные аргументы
-        with self.assertRaises(ValueError):
-            self.add_qq_q.execute([Rational(Natural([1]), Natural([2]))])  # Одного аргумента
-        with self.assertRaises(ValueError):
-            self.add_qq_q.execute([1, 2])  # Неправильные типы данных
+        r1 = Rational(Integer(Natural([1]), True), Natural([2]))  # 1/2
+        r2 = Rational(Integer(Natural([0]), True), Natural([1]))  # 0/1 (ноль)
+        result = self.add_qq_q.execute([r1, r2])[0]
+        num = Integer(Natural([1]), True)
+        den = Natural([2])
+        self.assertEqual(result.numerator, num)  # 1 (числитель)
+        self.assertEqual(result.denominator, den)  # 2 (знаменатель)
 
 if __name__ == '__main__':
     unittest.main()

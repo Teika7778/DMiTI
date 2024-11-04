ffb5db2966ca24720353524ac61d364060515e4b
Anastasiya Dorogushina <144445609+nastyanesty@users.noreply.github.com>
2024-11-04 19:11:42 +0300

    GCF_PP_P: так хотя бы тесты запускаются

--- core/GCF_PP_P.py
+++ core/GCF_PP_P.py
@@ -1,12 +1,11 @@
 from core import generic_module as gm
 from data_types import *
-from core.DEG_P_N import DEG_P_N
 from core.MOD_PP_P import MOD_PP_P
 import copy
 
+
 class GCF_PP_P(gm.AbstractModule):
     def __init__(self):
-        self.deg_p_n = DEG_P_N()
         self.mod_pp_p = MOD_PP_P()
 
     def execute(self, args):
@@ -14,15 +13,23 @@ class GCF_PP_P(gm.AbstractModule):
             raise ValueError("Improper arguments: function takes 2 arg")
         if not (isinstance(args[0], Polynomial) and isinstance(args[1], Polynomial)):
             raise ValueError("Invalid data type:  must be polynomial")
-        arg0_copy = copy.deepcopy(args[0]) # Копии многочленов
+        arg0_copy = copy.deepcopy(args[0])  # Копии многочленов
         arg1_copy = copy.deepcopy(args[1])
-        #Алгоритм Евклида делением
-        while( (self.deg_p_n(arg0_copy)[0] != 0) and (self.deg_p_n(arg1_copy)[0] != 0) ): # Пока степени обоих не равны нулю
-            if ( (self.deg_p_n(arg0_copy)[0] > self.deg_p_n(arg1_copy)[0])): # если степень первого больше
-                arg0_copy = self.mod_pp_p([arg0_copy, arg1_copy])[0] # делим на второй
-            else: #иначе
-                arg1_copy = self.mod_pp_p([arg1_copy, arg0_copy])[0] # делим на первый
-        if (self.deg_p_n(arg0_copy)[0] == 0):  # проверяем, какой не равен нулю
+        # Алгоритм Евклида делением
+        # Пока степени обоих не равны нулю
+        while len(arg0_copy.coefficients) > 1 and len(arg1_copy.coefficients) > 1:
+            # если степень первого больше
+            print([str(elem) for elem in arg0_copy.coefficients])
+            print([str(elem) for elem in arg1_copy.coefficients])
+            print("next")
+
+            if len(arg0_copy.coefficients) > len(arg1_copy.coefficients):
+                # делим на второй
+                arg0_copy = self.mod_pp_p.execute([arg0_copy, arg1_copy])[0]
+            else:  # иначе
+                arg1_copy = self.mod_pp_p.execute([arg1_copy, arg0_copy])[0]  # делим на первый
+
+        if len(arg0_copy.coefficients) == 1:  # проверяем, какой не равен нулю
             return [arg1_copy]
         else:
             return [arg0_copy]

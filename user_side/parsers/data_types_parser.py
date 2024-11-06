ccc4e011bc19b858d28120a33f067c986c172e14
Teika7778 <max2005swet@gmail.com>
2024-11-06 13:32:37 +0300

    задания+

--- user_side/parsers/data_types_parser.py
+++ user_side/parsers/data_types_parser.py
@@ -1,14 +1,6 @@
 """Преобразовываем строку к типам данных, действие обратное к __str__"""
 
 class DataTypeParser:
-    def str_to_natural(self, string):
+    def str_to_datatype(self, string):
         pass
 
-    def str_to_integer(self, string):
-        pass
-
-    def str_to_rational(self, string):
-        pass
-
-    def str_to_polynomial(self, string):
-        pass
